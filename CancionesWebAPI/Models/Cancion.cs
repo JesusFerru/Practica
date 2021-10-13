b4d85a850d178e430316ffb1fdad8de18faf5237
LuisFerrufino <a2020115239@estudiantes.upsa.edu.bo>
2021-10-13 17:07:42 -0400

    se agrego cancion.cs y appsetiting.json

--- CancionesWebAPI/Models/Cancion.cs
+++ CancionesWebAPI/Models/Cancion.cs
@@ -13,21 +13,15 @@ namespace CancionesWebAPI.Models
         [Display(Name = "Nombre de la Cancion")]
         public string NombreCancion { get; set; }
 
-
-
         [Required] //primero las condiciones
         [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese autor/compositor de la cancion")]
         [Display(Name = "Autor/Compositor")]
         public string Autor { get; set; } //luego se muestra que chota
 
-
-
         [Required]
         [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese Letra de la cancion")]
         [Display(Name = "Letra")]
         public string Letra { get; set; }
-
-
 
         [Url] //solo cuando es un link xd, caso contrario required
         [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese Enlace de la cancion")]
